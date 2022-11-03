b3bf219507c89d33fcf848fe899bb86e2584a0a1
mandolyte <cecil.new@gmail.com>
2022-11-03 11:33:55 -0400

    WIP usfm editor dependencies

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,9 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { AuthContext } from '@context/AuthContext'
 import { StoreContext } from '@context/StoreContext'
-import { usfm2perf } from '@utils/usfm2perf';
+// import { usfm2perf } from '@utils/usfm2perf';
 import { useRepoClient } from 'dcs-react-hooks';
-import EpiteleteHtml from "epitelete-html";
+// import EpiteleteHtml from "epitelete-html";
 import {usfmFilename} from '@common/BooksOfTheBible'
 import { decodeBase64ToUtf8 } from '@utils/base64Decode';
 import { LITERAL, SIMPLIFIED, CUSTOM } from '@common/constants';
@@ -22,7 +22,7 @@ export default function AppContextProvider({
   // const [ep, /*setEp*/] = useState(new EpiteletePerfHtml({
   //   proskomma: null, docSetId: "unfoldingWord/en_ltst", options: { historySize: 100 }
   // }))
-  const [ep, setEp] = useState({})
+  // const [ep, setEp] = useState({})
 
   const {
     state: {
@@ -115,32 +115,14 @@ export default function AppContextProvider({
             }
             _books[i].usfmText = _usfmText
             _books[i].type = ltStState
-            const _perf = usfm2perf(_usfmText)
-            if ( _perf === null ) {
-              _books[i].usfmText = null
-              _books[i].content = "CONTENT IS NOT USABLE"
-            } else {
-              _books[i].perf = _perf
-              const _docSetId = owner + "/" + _repo // captures org, lang, and type (literal or simplified)
-              _books[i].docset = _docSetId
-              if ( _ep[_docSetId] === undefined ) {
-                console.log("creating Epitelete for doc set:", _docSetId)
-                _ep[_docSetId] = new EpiteleteHtml({
-                  proskomma: null,
-                  docSetId: _docSetId,
-                  options: { historySize: 100 }
-                })
-              }
-              await _ep[_docSetId].sideloadPerf(_books[i].bookId.toUpperCase(), _books[i].perf)
-              console.log("epitelete docset,books:", _docSetId,_ep[_docSetId].localBookCodes())
-            }
+            const _docSetId = owner + "/" + _repo // captures org, lang, and type (literal or simplified)
+            _books[i].docset = _docSetId
           } else {
             _books[i].usfmText = null
           }
         }
       }
       setBooks(_books)
-      setEp(_ep)
       console.log("setBooks():",_books)
       setRefresh(false)
       setLtStState('')
@@ -186,3 +168,24 @@ AppContextProvider.propTypes = {
   ]).isRequired,
 };
 
+/* code graveyard
+
+            const _perf = usfm2perf(_usfmText)
+            if ( _perf === null ) {
+              _books[i].usfmText = null
+              _books[i].content = "CONTENT IS NOT USABLE"
+            } else {
+              _books[i].perf = _perf
+              const _docSetId = owner + "/" + _repo // captures org, lang, and type (literal or simplified)
+              _books[i].docset = _docSetId
+              if ( _ep[_docSetId] === undefined ) {
+                console.log("creating Epitelete for doc set:", _docSetId)
+                _ep[_docSetId] = new EpiteleteHtml({
+                  proskomma: null,
+                  docSetId: _docSetId,
+                  options: { historySize: 100 }
+                })
+              }
+              await _ep[_docSetId].sideloadPerf(_books[i].bookId.toUpperCase(), _books[i].perf)
+              console.log("epitelete docset,books:", _docSetId,_ep[_docSetId].localBookCodes())
+*/
