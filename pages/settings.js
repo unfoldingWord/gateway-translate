05e5667a4fdd3a0bc631e39262283317c1c8d43d
mandolyte <cecil.new@gmail.com>
2022-11-12 09:28:58 -0500

    WIP ckpt

--- pages/settings.js
+++ pages/settings.js
@@ -3,13 +3,15 @@ import { useRouter } from 'next/router'
 import Button from '@mui/material/Button'
 import SaveIcon from '@mui/icons-material/Save'
 import { AuthenticationContext } from 'gitea-react-toolkit'
+// import { AuthContext } from '@context/AuthContext'
 // import Layout from '@components/Layout'
 import TranslationSettings from '@components/TranslationSettings'
 
 const SettingsPage = () => {
   const router = useRouter()
   const { state: authentication } = useContext(AuthenticationContext)
-
+  // const { state: authentication } = useContext(AuthContext)
+  console.log("SettingsPage() authentication=", authentication)
   return (
     <div className='flex flex-col justify-center items-center'>
       <div className='flex flex-col w-full px-4 lg:w-132 lg:p-0'>
