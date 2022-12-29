e61c67b9c58bfb4ca76f914dfccebbf3576c20d0
kintsoogi <kintsoogii@proton.me>
2022-12-29 09:21:00 -0600

    Fixed export zip functionality to export user-edited usfm files

--- src/components/ScriptureWorkspace.js
+++ src/components/ScriptureWorkspace.js
@@ -103,8 +103,8 @@ function ScriptureWorkspace() {
   const [networkError, setNetworkError] = useState(null)
 
   const {
-    state: { books, ltStState },
-    actions: { setBooks, setLtStState },
+    state: { books },
+    actions: { setBooks },
   } = useContext(AppContext)
 
   const removeBook = id => {
@@ -114,18 +114,19 @@ function ScriptureWorkspace() {
     setBooks(_books)
   }
 
+  const handleUsfmUpdate = (id, updatedUsfmText) => {
+    const _books = books.map(book => {
+      if (book.id === id) {
+        return { ...book, usfmText: updatedUsfmText }
+      }
+      return book
+    })
+    setBooks(_books)
+  }
+
   const {
-    state: {
-      owner,
-      server,
-      appRef,
-      languageId,
-      currentLayout,
-      loggedInUser,
-      tokenNetworkError,
-      bibleReference,
-    },
-    actions: { logout, setCurrentLayout, setTokenNetworkError, setLastError },
+    state: { currentLayout, tokenNetworkError, bibleReference },
+    actions: { setCurrentLayout, setTokenNetworkError },
   } = useStoreContext()
 
   /**
@@ -166,57 +167,6 @@ function ScriptureWorkspace() {
     return null
   }
 
-  /**
-   * process error and determine if there is a problem with connection to server
-   *  if showAnyError is true we display an error popup
-   *    the process then is to check if this is server connection problem - if so we display that message, if not we display the error returned
-   *  if showAnyError is false (default) we only display an error popup if there is a problem connecting to server
-   * @param {string} message - the error message we received fetching a resource
-   * @param {boolean} isAccessError - if false then the error type is not one that would be caused by server connection problems
-   * @param {number} resourceStatus - status code returned fetching resource
-   * @param {object} error - error object for detected error, could be a parsing error, etc.  This will take precedence over message
-   * @param {boolean} showAllErrors - if true then always show a popup error message, otherwise just show server error message if we can't talk to server
-   */
-  function onResourceError(
-    message,
-    isAccessError,
-    resourceStatus,
-    error,
-    showAllErrors = false
-  ) {
-    if (!networkError) {
-      // only show if another error not already showing
-      if (showAllErrors) {
-        processNetworkError(
-          error || message,
-          resourceStatus,
-          logout,
-          router,
-          setNetworkError,
-          setLastError,
-          setLastError
-        )
-      } else {
-        if (isAccessError) {
-          // we only show popup for access errors
-          addNetworkDisconnectError(
-            error || message,
-            0,
-            logout,
-            router,
-            setNetworkError,
-            setLastError
-          )
-        }
-      }
-    }
-  }
-
-  const config = {
-    server,
-    ...HTTP_CONFIG,
-  }
-
   if (tokenNetworkError || networkError) {
     return (
       <>
@@ -236,6 +186,7 @@ function ScriptureWorkspace() {
         docSetId={data.docset}
         data={data}
         classes={classes}
+        onSave={handleUsfmUpdate}
         onClose={removeBook}
       />
     ))
@@ -271,3 +222,67 @@ function ScriptureWorkspace() {
 }
 
 export default ScriptureWorkspace
+
+// DCS CODE GRAVEYARD
+
+/**
+ * process error and determine if there is a problem with connection to server
+ *  if showAnyError is true we display an error popup
+ *    the process then is to check if this is server connection problem - if so we display that message, if not we display the error returned
+ *  if showAnyError is false (default) we only display an error popup if there is a problem connecting to server
+ * @param {string} message - the error message we received fetching a resource
+ * @param {boolean} isAccessError - if false then the error type is not one that would be caused by server connection problems
+ * @param {number} resourceStatus - status code returned fetching resource
+ * @param {object} error - error object for detected error, could be a parsing error, etc.  This will take precedence over message
+ * @param {boolean} showAllErrors - if true then always show a popup error message, otherwise just show server error message if we can't talk to server
+ */
+// function onResourceError(
+//   message,
+//   isAccessError,
+//   resourceStatus,
+//   error,
+//   showAllErrors = false
+// ) {
+//   if (!networkError) {
+//     // only show if another error not already showing
+//     if (showAllErrors) {
+//       processNetworkError(
+//         error || message,
+//         resourceStatus,
+//         logout,
+//         router,
+//         setNetworkError,
+//         setLastError,
+//         setLastError
+//       )
+//     } else {
+//       if (isAccessError) {
+//         // we only show popup for access errors
+//         addNetworkDisconnectError(
+//           error || message,
+//           0,
+//           logout,
+//           router,
+//           setNetworkError,
+//           setLastError
+//         )
+//       }
+//     }
+//   }
+// }
+
+// const config = {
+//   server,
+//   ...HTTP_CONFIG,
+// }
+
+// const {
+//   state: {
+//     owner,
+//     server,
+//     appRef,
+//     languageId,
+//     loggedInUser,
+//   },
+//   actions: { logout setLastError },
+// } = useStoreContext()
--- src/components/ScriptureWorkspaceCard.js
+++ src/components/ScriptureWorkspaceCard.js
@@ -14,6 +14,7 @@ export default function ScriptureWorkspaceCard({
   docSetId,
   data,
   classes,
+  onSave: saveToWorkspace,
   onClose: removeBook,
 }) {
   const [doSave, setDoSave] = useState(false)
@@ -40,6 +41,7 @@ export default function ScriptureWorkspaceCard({
     }
     if (doSave) {
       console.log('New updated USFM:', doSave)
+      saveToWorkspace(id, doSave)
       saveContent()
     }
   }, [
